a1089add878cc2f366c6337c0a249a97965bcbb4
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-09-23 06:10:00 +0600

    edit

--- js/coutries.js
+++ js/coutries.js
@@ -1,10 +1,10 @@
-const loadCountries = () =>{
+const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => displayCountries(data));
+        .then(res => res.json())
+        .then(data => displayCountries(data));
 }
 
-const displayCountries = countries =>{
+const displayCountries = countries => {
     // console.log(countries[0]);
     const countriesHTML = countries.map(country => getCountryHTML(country));
     // console.log(countriesHTML[0]);
@@ -13,12 +13,14 @@ const displayCountries = countries =>{
 }
 
 // option 3
-const getCountryHTML = ({name, flags, area, region}) =>{
+const getCountryHTML = ({ name, flags, area, region, timezones, subregion, }) => {
     return `
         <div class="country">
             <h2>${name.common}</h2>
             <p>Area: ${area}</p>
             <p>Continet: ${region}</p>
+            <p>timezones: ${timezones}</p>
+            <p>subregion: ${subregion}</p>
             <img src="${flags.png}">
         </div>
     `
